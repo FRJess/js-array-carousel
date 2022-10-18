ab0f237472a15a030653cc87388bee5c3a59a8be
FR_Jess <fr_jess@icloud.com>
2022-10-18 10:32:29 +0200

    fix

--- Milestone2/js/script.js
+++ Milestone2/js/script.js
@@ -7,11 +7,14 @@ const imagesArray = [
   "04.jpg",
   "05.jpg",
 ];
-
 //add images to html file
 const itemsWrapper = document.querySelector(".items-wrapper");
-
 let imagesTags = " ";
+let counterImages = 0;
+// buttons
+const next = document.querySelector(".down");
+const previous = document.querySelector(".up");
+const items = document.getElementsByClassName("item");
 
 // Conditional statement for images visualization
 for(let i = 0; i < imagesArray.length; i++){
@@ -21,35 +24,39 @@ for(let i = 0; i < imagesArray.length; i++){
   `;
 }
 
-let counterImages = 0;
-
-// buttons
-const next = document.querySelector(".down");
-const previous = document.querySelector(".up");
 previous.classList.add("hide")
 
 //imageTags
 itemsWrapper.innerHTML = imagesTags;
 
-const items = document.getElementsByClassName("item");
-
 items[counterImages].classList.add("active");
 
-//FUNCTION CLICK CHANGE IMG
+//FUNCTION CLICK CHANGE NEXT IMG
 next.addEventListener("click", function(){
+
+  //active class change
   items[counterImages].classList.remove("active");
   items[++counterImages].classList.add("active");
 
+  // LOOP INFINITO NON FUNZIONA
+  // if(counterImages === imagesArray.length -1) counterImages = -1;
+  // items[counterImages].classList.add("active");
+
+  //hide class change
   previous.classList.remove("hide");
   if(counterImages === imagesArray.length -1){
     next.classList.add("hide");
   }
 });
 
+//FUNCTION CLICK CHANGE PREVIOUS IMG
 previous.addEventListener("click", function(){
+
+  //active class change
   items[counterImages].classList.remove("active");
   items[--counterImages].classList.add("active");
 
+  //hide class change
   next.classList.remove("hide");
   if(counterImages === 0){
     previous.classList.add("hide");
