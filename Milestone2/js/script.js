a716588df6507339a99a6b95d2063f132bccefe1
FR_Jess <fr_jess@icloud.com>
2022-10-18 16:01:59 +0200

    fix

--- Milestone2/js/script.js
+++ Milestone2/js/script.js
@@ -1,64 +1,51 @@
 
 // VARIABLES DECLARATION AND INITIALIZATION
-const imagesArray = [
-  "01.jpg",
-  "02.jpg",
-  "03.jpg",
-  "04.jpg",
-  "05.jpg",
-];
+
 //add images to html file
-const itemsWrapper = document.querySelector(".items-wrapper");
-let imagesTags = " ";
+const slider = document.querySelector('.slider');
+const thumbs = document.querySelector('.thumbs');
+const next = document.querySelector('.up');
+const prev = document.querySelector('.down');
+
+const numImages = 5;
 let counterImages = 0;
-// buttons
-const next = document.querySelector(".down");
-const previous = document.querySelector(".up");
-const items = document.getElementsByClassName("item");
+let sliderHtml = '';
+let thumbsHtml = '';
+
 
 // Conditional statement for images visualization
-for(let i = 0; i < imagesArray.length; i++){
-
-  imagesTags += `
-    <img src="img/${imagesArray[i]}" alt="${imagesArray[i]}" class="item">
+for(let i = 1; i <= numImages; i++){
+  sliderHtml += `
+      <img  class="item" src="img/0${i}.jpg" alt="">
+  `;
+  thumbsHtml += `
+      <img  class="item-thumb" src="img/0${i}.jpg" alt="">
   `;
 }
 
-previous.classList.add("hide")
+slider.innerHTML = sliderHtml;
+thumbs.innerHTML = thumbsHtml;
 
-//imageTags
-itemsWrapper.innerHTML = imagesTags;
+const listImages = document.getElementsByClassName('item');
+const listthumbs = document.getElementsByClassName('item-thumb');
 
-items[counterImages].classList.add("active");
+listImages[counterImages].classList.add('active');
+listthumbs[counterImages].classList.add('active');
 
-//FUNCTION CLICK CHANGE NEXT IMG
-next.addEventListener("click", function(){
+prev.addEventListener('click',function(){
+  listImages[counterImages].classList.remove('active');
+  listthumbs[counterImages].classList.remove('active');
+  counterImages++;
+  if(counterImages === numImages) counterImages = 0;
+  listImages[counterImages].classList.add('active');
+  listthumbs[counterImages].classList.add('active');
+})
 
-  //active class change
-  items[counterImages].classList.remove("active");
-  items[++counterImages].classList.add("active");
-
-  // LOOP INFINITO NON FUNZIONA
-  // if(counterImages === imagesArray.length -1) counterImages = -1;
-  // items[counterImages].classList.add("active");
-
-  //hide class change
-  previous.classList.remove("hide");
-  if(counterImages === imagesArray.length -1){
-    next.classList.add("hide");
-  }
-});
-
-//FUNCTION CLICK CHANGE PREVIOUS IMG
-previous.addEventListener("click", function(){
-
-  //active class change
-  items[counterImages].classList.remove("active");
-  items[--counterImages].classList.add("active");
-
-  //hide class change
-  next.classList.remove("hide");
-  if(counterImages === 0){
-    previous.classList.add("hide");
-  }
-});
+next.addEventListener('click',function(){
+  listImages[counterImages].classList.remove('active');
+  listthumbs[counterImages].classList.remove('active');
+  counterImages--;
+  if(counterImages < 0) counterImages = numImages - 1;
+  listImages[counterImages].classList.add('active');
+  listthumbs[counterImages].classList.add('active');
+})
